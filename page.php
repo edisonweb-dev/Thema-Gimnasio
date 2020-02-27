e0572832970379749964b68602ab0e5d20171916
Edison Perdomo <edisonperdomodev@gmail.com>
2020-02-27 07:38:24 -0400

    Añadiendo los estilos css thema

---
+++ page.php
@@ -0,0 +1,23 @@
+
+<?php get_header(); ?>
+
+<?php
+
+  while( have_posts() ):
+
+    the_post();
+?>
+
+<h1><?php the_title() ?></h1>
+
+<?php the_content() ?>
+
+Escrito por: <?php the_author() ?>
+
+Escrito fecha <?php the_date() ?>
+
+<?php
+  endwhile;  
+?>
+
+<?php get_footer(); ?>
